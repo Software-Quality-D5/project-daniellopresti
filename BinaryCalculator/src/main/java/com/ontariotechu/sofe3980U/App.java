c000b57d00f27283dc24b4e11ebc439250aef9fc
icekoda <icekoda@protonmail.com>
2023-01-26 16:02:18 -0500

    added files

---
+++ BinaryCalculator/src/main/java/com/ontariotechu/sofe3980U/App.java
@@ -0,0 +1,38 @@
+package com.ontariotechu.sofe3980U;
+
+import org.joda.time.LocalTime;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	/**
+	* Main program:  The entry point of the program. The local time will be printed first,
+	*      Then it will create two binary variables, add them and print the result.
+	*
+	* @param args: not used
+	*/
+    public static void main( String[] args )
+    {
+		LocalTime currentTime = new LocalTime();
+		System.out.println("The current local time is: " + currentTime);
+		Binary binary1=new Binary("00010001000");
+        System.out.println( "First binary number is "+binary1.getValue());
+		Binary binary2=new Binary("111000");
+        System.out.println( "Second binary number is "+binary2.getValue());
+		Binary sum= Binary.add(binary1,binary2);
+		System.out.println( "Their summation is "+sum.getValue());
+
+		Binary binary3=new Binary("10101010");
+		Binary binary4=new Binary("11010011");
+
+		Binary or= Binary.or(binary3, binary4);
+		System.out.println("Or produced: " + or.getValue());
+
+	//	Binary and=Binary.and(binary3,binary4);
+	//	System.out.Println("And produced: " + and.getValue());
+
+    }
+}
